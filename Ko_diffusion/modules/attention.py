2a6b71a8597a64a157ce57d1e6dadc2e0b753a73
o2rabbit <gih5444@naver.com>
2023-07-09 15:43:24 +0900

    Add: Basic Trasformer block and Trasformer block

--- Ko_diffusion/modules/attention.py
+++ Ko_diffusion/modules/attention.py
@@ -3,22 +3,36 @@ import torch.nn.functional as F
 from torch import nn, einsum
 
 
+class GEGLU(nn.Module):
+    """
+    https://arxiv.org/abs/2002.05202v1
+    """
+    def __init__(self, dim_in, dim_out):
+        super().__init__()
+        self.proj = nn.Linear(dim_in, dim_out*2)
+
+    def forward(self, x):
+        x, gate = self.proj(x).chunk(2, diim=-1)
+        return x * F.gelu(gate)
+
+
 class Attention(nn.Module):
+    """
+    Attention Block for Self-Attention and Cross-Attention.
+    :query_dim: Dimension of query
+    :context_dim: Dimension of context, None parameter works as Self-Attention (default=None)
+    :num_heads: Number of Head (default=4)
+    :head_dim: Dimension of Head, None parameter define head_dim to query_dim (default=None)
+    :dropout: DropOut Rate (default=0.)
+    """
     def __init__(self,
                  query_dim: int,
                  context_dim: int = None,
                  num_heads: int = 4,
                  head_dim: int = None,
-                 dropout: float = 0.):
+                 dropout: float = 0.
+                 ):
         super().__init__()
-        """
-        Attention Block for Self-Attention and Cross-Attention
-        :query_dim: Dimension of query
-        :context_dim: Dimension of context, None parameter works as Self-Attention (default=None)
-        :num_heads: Number of Head (default=4)
-        :head_dim: Dimension of Head, None parameter define head_dim to query_dim (default=None)
-        :dropout: DropOut Rate (default=0.)
-        """
         assert query_dim 
 
         if not head_dim:
@@ -65,5 +79,99 @@ class Attention(nn.Module):
         return self.to_out(out)
 
 
+class BasicTransformerBlock(nn.Module):
+    """
+    Basic Trasformer Block include self-attention, cross-attention, feedforward network.
+    :dim: Demension of input tensor
+    :num_heads: Number of Attention Head 
+    :head_dim: Dimension of Attention Head
+    :context_dim: Dimension of context
+    :ff_dim_mult: Multiply Value of Feed Forward Network Inner Dimension (default=4)
+    :use_GEGLU: (default=True)
+    :dropout: DropOut Rate (default=0.)
+    """
+    def __init__(self,
+                 dim: int,
+                 num_heads: int,
+                 head_dim: int,
+                 context_dim: int = None,
+                 ff_dim_mult: int = 4,
+                 use_GEGLU: bool = True,
+                 dropout: float = 0.,
+                 ):
+        super().__init__()
+        ff_inner_dim = int(dim * ff_dim_mult)
+        ff_layer = [nn.Linear(dim, ff_inner_dim), nn.GELU()] if not use_GEGLU else [GEGLU(dim, ff_inner_dim)]
+
+        self.attn1 = Attention(query_dim=dim, num_heads=num_heads, head_dim=head_dim, dropout=dropout) # self-attention
+        self.attn2 = Attention(query_dim=dim, context_dim=context_dim, num_heads=num_heads, head_dim=head_dim, dropout=dropout) # cross-attention
+
+        self.ff = nn.Sequential(
+            *ff_layer,
+            nn.Dropout(p=dropout),
+            nn.Linear(ff_inner_dim, dim)
+        )
+
+        self.norm1 = nn.LayerNorm(dim)
+        self.norm2 = nn.LayerNorm(dim)
+        self.norm3 = nn.LayerNorm(dim)
+
+    def forward(self, x, context=None):
+        x = self.attn1(self.norm1(x)) + x
+        x = self.attn2(self.norm2(x), context=context) + x
+        x = self.ff(self.norm3(x)) + x
+        return x
 
 
+class SpatialTrasformerBlock(nn.Module):
+    """
+    Trasformer block for image-like data.
+    """
+    def __init__(self, in_channels, num_head, head_dim, depth, dropout, context_dim):
+        super().__init__()
+
+    def forward(self, x, context=None):
+        return x
+    
+
+class TrasformerBlock(nn.Module):
+    """
+    Trasformer Block for Unet.
+    :in_channels: Number of input tensor channels 
+    :num_heads: Number of Attention Head 
+    :head_dim: Dimension of Attention Head
+    :context_dim: Dimension of context
+    :depth: Number of Transformer block Depth (default=1)
+    :dropout: DropOut Rate (default=0.)
+    :use_spatial: use Spatial Trasformer Block 
+    """
+    def __init__(self, 
+                 in_channels: int, 
+                 num_heads: int, 
+                 head_dim: int, 
+                 context_dim: int = None,
+                 depth: int = 1, 
+                 dropout: float = 0., 
+                 use_spatial: bool = False,
+                 ):
+        super().__init__()
+        self.use_spatial = use_spatial
+        self.norm = nn.GroupNorm(num_groups=32, num_channels=in_channels, eps=1e-6, affine=True)
+
+        self.transformer_block = nn.ModuleList([])
+        if use_spatial: # not use currently 
+            self.trasformer_block.append(SpatialTrasformerBlock(in_channels, num_heads, head_dim, depth, dropout, context_dim))
+        else:
+            for _ in range(depth):
+                self.transformer_block.append(BasicTransformerBlock(in_channels, num_heads, head_dim, dropout, context_dim))
+
+    def forward(self, x, context=None):
+        b, c, h, w = x.shape
+        x_in = x
+        x = self.norm(x) if self.use_spatial else x
+        x = x.permute(0, 2, 3, 1).view(b, -1, c) # b c h w -> b (h w) c
+        for block in self.transformer_block:
+            x = block(x, context=context)
+        x = x.view(b, h, w, c).permute(0, 3, 1, 2) # b (h w) c -> b c h w
+        x = x + x_in if self.use_spatial else x
+        return x
