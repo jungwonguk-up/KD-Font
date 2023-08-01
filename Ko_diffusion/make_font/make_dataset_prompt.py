8c0b5415d30cb437d2d0b8aefd055acabda17dbd
internationalwe <tlsghwns1122@gmail.com>
2023-08-01 15:37:16 +0900

    FEATURE : ADD goal of stroke amount and count sum of second class value

--- Ko_diffusion/make_font/make_dataset_prompt.py
+++ Ko_diffusion/make_font/make_dataset_prompt.py
@@ -28,11 +28,11 @@ def make_letter_image(image_size, letter):
 
 
 class windows_tkinter:
-    def __init__(self,start_number,ch_letters,image_size=100):
+    def __init__(self,start_number,ch_letters_sum,image_size=100):
         self.window = Tk()
         self.start_number= start_number
         self.current_number = start_number
-        self.ch_letters = ch_letters
+        self.ch_letters_sum = ch_letters_sum
         self.image_size = image_size
         
         self.class_width = 0.075
@@ -54,8 +54,10 @@ class windows_tkinter:
         self.click_category = 100
         self.click_class = 100
         self.click_second_class = 100
+        self.second_class_click_number = 0
         self.second_class_value_info_on = False
         self.letter_image_label = None
+        self.counting_label = None
     
     def clear_button_action(self):
         for scbdx in range(32):
@@ -66,16 +68,17 @@ class windows_tkinter:
         self.click_category = 100
         self.click_class = 100
         self.click_second_class = 100
+        self.second_class_click_number = 0
         self.second_class = [[0 for _ in range(32)] for _ in range(11)]
 
     def next_button_action(self):
-        with open('ch_embedding.txt','a',encoding='utf-8') as f:
-            letter_embedding = f'{self.ch_letters[self.current_number]} {self.second_class}\n'
+        with open('ch_label.txt','a',encoding='utf-8') as f:
+            letter_embedding = f'{self.ch_letters_sum[self.current_number][0]} {self.second_class}\n'
             f.write(letter_embedding)
         
         self.clear_button_action()
         self.current_number += 1
-        letter_img = make_letter_image(self.image_size*2,self.ch_letters[self.current_number])
+        letter_img = make_letter_image(self.image_size*2,self.ch_letters_sum[self.current_number][0])
         letter_image = ImageTk.PhotoImage(letter_img)
         self.letter_image_label['image'] = letter_image
         self.letter_image_label.image=letter_image
@@ -91,10 +94,12 @@ class windows_tkinter:
             self.second_class_value_info[scbdx].place(relx = 0.25 + (scbdx % 8) * 0.09,rely=0.6+(scbdx //8)*0.09)
         print('first')
 
-    def update_second_class_value_info_WSC(self,scdx):
+    def update_second_class_value_info_WSCC(self,scdx):
         self.click_second_class = scdx
         self.second_class[self.click_class][self.click_second_class] += 1
         self.second_class_value_info[self.click_second_class]['text'] = self.second_class[self.click_class][self.click_second_class]
+        self.second_class_click_number +=1
+        self.counting_label['text'] = f"{self.second_class_click_number}획"
 
     def display_second_class_value_info_WCC(self,cdx):
         self.click_class = cdx
@@ -111,7 +116,6 @@ class windows_tkinter:
         
         for scvdx in range(32):
             self.second_class_value_info[scvdx]['text'] = self.second_class[self.click_class][scvdx]
-    
 
         
     # def clear
@@ -153,14 +157,18 @@ class windows_tkinter:
 
 
 
-
-
-        letter_img = make_letter_image(self.image_size*2,self.ch_letters[self.start_number])
+        
+        letter_img = make_letter_image(self.image_size*2,self.ch_letters_sum[self.start_number][0])
         letter_image = ImageTk.PhotoImage(letter_img)
         self.letter_image_label = Label(self.window,image=letter_image)
         self.letter_image_label.image=letter_image
         self.letter_image_label.place(relx=0.02,rely=0.21,relwidth=0.2)
-
+        
+        self.goal_counting_label = Label(self.window,text=f"{self.ch_letters_sum[self.current_number][1]}획",width = int(self.image_size*0.2),height=int(self.image_size*0.06),font=('나눔바른펜',int(self.image_size*0.1)),bg=BTN_COLOR)
+        self.goal_counting_label.place(relx=0.02,rely=0.65,relwidth=0.2,relheight=0.06)
+        self.counting_label = Label(self.window,text=f"0획",width = int(self.image_size*0.2),height=int(self.image_size*0.06),font=('나눔바른펜',int(self.image_size*0.1)),bg=BTN_COLOR)
+        self.counting_label.place(relx=0.02,rely=0.75,relwidth=0.2,relheight=0.06)
+        
         next_button = Button(self.window,text=f"NEXT",width = int(self.image_size*0.2),height=int(self.image_size*0.06),font=('나눔바른펜',int(self.image_size*0.1)),bg=BTN_COLOR,command=lambda:self.next_button_action())
         next_button.place(relx=0.02,rely=0.02,relwidth=0.2,relheight=0.06)
         clear_button = Button(self.window,text=f"CLEAR",width = int(self.image_size*0.2),height=int(self.image_size*0.06),font=('나눔바른펜',int(self.image_size*0.1)),bg=BTN_COLOR,command=lambda:self.clear_button_action())
@@ -178,7 +186,7 @@ class windows_tkinter:
         
         for scdx in range(32):
             self.second_class_images[scdx] = ImageTk.PhotoImage(Image.open(os.path.join(ch_label_path,ch_label_image_name[scdx])).resize((int(self.image_size*0.8),int(self.image_size*0.8)), Image.ANTIALIAS))
-            self.second_class_buttons[scdx] = Button(self.window,image=self.second_class_images[scdx],command=lambda svdx = scdx : self.update_second_class_value_info_WSC(svdx))
+            self.second_class_buttons[scdx] = Button(self.window,image=self.second_class_images[scdx],command=lambda svdx = scdx : self.update_second_class_value_info_WSCC(svdx))
             self.second_class_buttons[scdx].place(relx = 0.25 + (scdx % 8) * 0.09,rely=0.2+(scdx //8)*0.09)
         for scbdx in range(32):
             self.second_class_value_info[scbdx] = Label(self.window,text=0,width = int(self.image_size*0.1),height=int(self.image_size*0.08),font=('나눔바른펜',int(self.image_size*0.1)),bg=BTN_COLOR)
@@ -187,10 +195,12 @@ class windows_tkinter:
 
 
 if __name__ == "__main__":
-    ch_letters = ""
-    with open('ch_letter.txt','r',encoding='utf-8') as f:
-        ch_letters = f.read()
-    ss = windows_tkinter(1000,ch_letters,100)
+    ch_letters_sum = []
+    with open('ch_letter_sum.txt','r',encoding='utf-8') as f:
+        for line in f.readlines():
+            ch_letters_sum.append(line.split())
+    # print(ch_letters_sum)
+    ss = windows_tkinter(start_number=1000,ch_letters_sum=ch_letters_sum,image_size=100)
     ss.display_window()
 
 
