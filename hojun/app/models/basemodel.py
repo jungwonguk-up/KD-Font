b580012cead534afc906f1679bc828c45a4e9637
o2rabbit <gih5444@naver.com>
2023-10-25 19:02:57 +0900

    Add: routes and models

--- hojun/app/models/basemodel.py
+++ hojun/app/models/basemodel.py
@@ -16,8 +16,26 @@ class UserRequest(Document):
             "example": {"id": "1234 (uuid)",
                         "email": "abc@naver.com",
                         "original_image_path": ".../images/original_img.png",
-                        "cropped_image_path:": ".../images/cropped_img.png",
+                        "cropped_image_path": ".../images/cropped_img.png",
                         "sampling_images_path": [".../images/sample1.png", ".../images/sample2.png"],
-                        "example_images_path": ".../images/example_img.png"}
+                        "example_image_path": ".../images/example_img.png"}
+        }
+    }
+
+
+class UserRequestUpdate(BaseModel):
+    email: EmailStr
+    original_image_path: Optional[str] = None
+    cropped_image_path: Optional[str] = None
+    sampling_images_path: Optional[List[str]] = None
+    example_image_path: Optional[str] = None
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {"email": "abc@naver.com",
+                        "original_image_path": ".../images/original_img.png",
+                        "cropped_image_path": ".../images/cropped_img.png",
+                        "sampling_images_path": [".../images/sample1.png", ".../images/sample2.png"],
+                        "example_image_path": ".../images/example_img.png"}
         }
     }
--- hojun/app/routers/route.py
+++ hojun/app/routers/route.py
@@ -5,10 +5,11 @@ import uuid
 from pathlib import Path
 
 from fastapi import APIRouter, Form, UploadFile, File, HTTPException, status
+from fastapi.responses import FileResponse
 from beanie import PydanticObjectId
 
 from database.db import Database
-from models.basemodel import UserRequest
+from models.basemodel import UserRequest, UserRequestUpdate
 from library.func import get_storage_path, save_image, read_image
 
 from typing import List
@@ -26,7 +27,7 @@ async def request_rest(id: str, image: bytes):
         headers=headers,
     )
 
-    return dict(response.json())['outputs'][0]
+    return None #TODO 전송 결과값을 받아야 한다
 
 
 user_router = APIRouter()
@@ -43,19 +44,86 @@ async def create_inference_request(email: str = Form(...), image_file: UploadFil
     image_path = storage_path / Path("ori.png") #TODO 확장자는 따로 지정해줘야하나?
     save_image(image_file, str(storage_path/"ori"))
     
-    # new db
+    # new db 
     user_request = UserRequest(
         id=user_id,
         email=email,
-        original_image_path=str(image_path))
-    
-    print(f"id: {user_request.id}, email: {user_request.email}, ori_path: {user_request.original_image_path}")
+        original_image_path=str(image_path)
+    )
 
     await requests_database.save(user_request)
+    print(f"Request ID {user_id[:-8]} recoded to DB sucessfully.")
 
     #TODO Request
+    # response = await request_rest(id=user_id,
+    #                               image=image_file,)
+    # if response:
 
     return {"message": "request info created sucessfully."}
+
+
+@user_router.get("/{id}/status")
+async def get_request_status(id: str):
+    user_request = await requests_database.get(id)
+    
+    if not user_request:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="user_request info with ID dose not exist."
+        )
+    
+    if user_request.cropped_image_path is None and user_request.sampling_images_path is None:
+        return {"message": "Preprocessing"}
+    elif user_request.cropped_image_path is not None and user_request.sampling_images_path is None:
+        return {"message": "Sampling"}
+    elif user_request.sampling_images_path is not None:
+        return {"message": "Completed"}
+
+
+#TODO # 샘플링 이미지 생성 완료 신호 받기
+@user_router.post("/{id}")
+async def receive_sampling_complete_signal(id: str) -> dict:
+    user_request = await requests_database.get(id)
+    
+    sampling_images_path_list = user_request.sampling_images_path
+
+    print(sampling_images_path_list)
+
+    #TODO make example image and save to db
+
+    return {"message": "signal received successfully."}
+
+
+#TODO 샘플 이미지 받기
+@user_router.get("/{id}/sample_image")
+async def get_sampled_image(id: str):
+    user_request = await requests_database.get(id)
+
+
+
+    #TODO return multiple images
+    return FileResponse()
+
+
+@user_router.get("/{id}/example_image")
+async def get_example_image(id: str):
+    user_request = await requests_database.get(id)
+
+    if not user_request:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="user_request info with ID dose not exist."
+        )
+    # get example image path from db
+    example_image_path = user_request.example_image_path
+
+    if not example_image_path:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Sample image path with ID dose not exist."
+        )
+    
+    return FileResponse(path=example_image_path,)
 
 
 @user_router.get("/{id}", response_model=UserRequest)
@@ -76,6 +144,24 @@ async def get_all_requests() -> List[UserRequest]:
     user_requests = await requests_database.get_all()
 
     return user_requests
+
+
+#TODO should fix
+@user_router.put("/{id}", response_model=UserRequest)
+async def update_request(id: str, body: UserRequestUpdate) -> UserRequest:
+    print("UserRequest")
+    print(body)
+    print()
+    
+    user_request = await requests_database.update(id=id, body=body)
+
+    if not user_request:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="user_request info with ID dose not exist."
+        )
+
+    return user_request
 
 
 @user_router.delete("/{id}")
