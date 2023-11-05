71577c9e22af8487f88232a8fa7690b16c0e27e6
internationalwe <tlsghwns1122@gmail.com>
2023-11-05 11:58:52 +0000

    "Add : Config yaml at train & test"

--- ML/train.py
+++ ML/train.py
@@ -29,56 +29,57 @@ import wandb
 # seed = 7777
 
 # graphic number #####
-gpu_num = 0 #####
-input_size = 64
-batch_size = 8 #####
-num_classes = 11172 #####
-lr = 3e-4 #####
-n_epochs = 300 #####
-start_epoch = 10
-mode = 2 # mode_1 : with contents, stroke, mode_2 : with contents, stroke, style #####
-sampling_chars = "괴그기깅나는늘다도디러로를만버없에우워을자점하한했"
-noise_step = 1000
-cfg_scale = 3
+# config['gpu_num'] = 0 #####
+# config['input_size'] = 64
+# config['batch_size'] = 8 #####
+# config['num_classes'] = 11172 #####
+# config['lr'] = 3e-4 #####
+# config['n_epochs'] = 300 #####
+# config['start_epoch'] = 10
+# config['mode'] = 2 # mode_1 : with contents, stroke, mode_2 : with contents, stroke, style #####
+# config['sampling_chars'] = "괴그기깅나는늘다도디러로를만버없에우워을자점하한했"
+# config['noise_step'] = 1000
+# config['cfg_scale'] = 3
 
 
-resume_train = False
-
-train_dirs = '/home/hojun/Documents/code/cr_diffusion/KD-Font/Tools/MakeFont/Hangul_Characters_Image64_Grayscale'
-sample_img_path = f'{train_dirs}/62570_늘.png'
-
-csv_path = "/home/hojun/Documents/code/cr_diffusion/KD-Font/Tools/MakeFont/diffusion_font_train.csv"
-style_path = "/home/hojun/Documents/code/cr_diffusion/KD-Font/ML/style_enc.pth"
+# config['resume_train'] = False
 
 
-# torch.multiprocessing.set_start_method('forkserver',force=True)
+
 if __name__ == '__main__':
     # Load config
     config_path = os.path.join(prj_dir, 'config', 'train.yaml')
     config = load_yaml(config_path)
     
-    file_number= "Unet64_image420_5"
-    result_image_path = os.path.join("results", "images", 'font_noStrokeStyle_{}'.format(file_number))
-    result_model_path = os.path.join("results", "models", 'font_noStrokeStyle_{}'.format(file_number))
+    # Set path
+    train_dirs = os.path.join(prj_dir,config['train_dirs'])
+    sample_img_path  = os.path.join(prj_dir,config['sample_img_path'])
+    csv_path = os.path.join(prj_dir,config['csv_path'])
+    style_path = os.path.join(prj_dir,config['style_path'])
+    
+    result_image_path = os.path.join(prj_dir,config['result_path'], "images", config['folder_name'])
+    result_model_path = os.path.join(prj_dir,config['result_path'], "models", config['folder_name'])
+    
     if os.path.exists(result_model_path):
         print("file_exist")
         exit()
     os.makedirs(result_image_path, exist_ok=True)
     os.makedirs(result_model_path, exist_ok=True)
 
-    # wandb init
-    # wandb.init(project="diffusion_font_test", config={
-    #             "learning_rate": 0.0003,
-    #             "architecture": "UNET",
-    #             "dataset": "HOJUN_KOREAN_FONT64",
-    #             "notes":"content, yes_stoke, non_style/ 64 x 64, 420 dataset"
-    #             },
-    #            name = "self-attetnion condtion content stroke GAP 25") #####
-    wandb.init(mode="disabled")
+    if config['wandb']:
+        wandb.init(project="diffusion_font_test", config={
+                    "learning_rate": 0.0003,
+                    "architecture": "UNET",
+                    "dataset": "HOJUN_KOREAN_FONT64",
+                    "notes":"content, yes_stoke, non_style/ 64 x 64, 420 dataset"
+                    },
+                   name = "self-attetnion condtion content stroke GAP 25")
+    else:
+        wandb.init(mode="disabled")
 
 
     # Set device(GPU/CPU)
-    os.environ['CUDA_VISIBLE_DEVICES'] = str(gpu_num)
+    os.environ['CUDA_VISIBLE_DEVICES'] = str(config['gpu_num'])
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     # Set data directory
@@ -91,33 +92,35 @@ if __name__ == '__main__':
         torchvision.transforms.ToTensor(),
         torchvision.transforms.Normalize((0.5), (0.5))
     ])
+    
     train_dataset = DiffusionDataset(csv_path=csv_path,transform=transforms)
-    sampling_dataset = DiffusionSamplingDataset(sampling_img_path=sample_img_path,sampling_chars=sampling_chars,img_size=input_size,device=device,transforms=transforms )
+    sampling_dataset = DiffusionSamplingDataset(sampling_img_path=sample_img_path,sampling_chars=config['sampling_chars'],img_size=config['input_size'],device=device,transforms=transforms )
+    
     # test set
     # n = range(0,len(dataset),10)
     # dataset = Subset(dataset, n)
 
-    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=12)
-    test_dataloader = DataLoader(sampling_dataset,batch_size=batch_size,shuffle=False)
+    train_dataloader = DataLoader(train_dataset, batch_size=config['batch_size'], shuffle=True, num_workers=12)
+    # test_dataloader = DataLoader(sampling_dataset,batch_size=config['batch_size'],shuffle=False)
     
     sample_img = Image.open(sample_img_path)
     sample_img = transforms(sample_img).to(device)
     sample_img = torch.unsqueeze(sample_img,1)
-    sample_img = sample_img.repeat(len(sampling_chars), 1, 1, 1)
+    sample_img = sample_img.repeat(len(config['sampling_chars']), 1, 1, 1)
 
     
-    if resume_train:
+    if config['resume_train']:
         #Set model
         model = UNet()
 
         #Set optimizer
-        optimizer = optim.AdamW(model.parameters(), lr=lr)
+        optimizer = optim.AdamW(model.parameters(), lr=config['lr'])
 
         #load weight
-        model.load_state_dict(torch.load('./models/font_noStrokeStyle_Unet64_image64_2/ckpt_90.pt'))
+        model.load_state_dict(torch.load(config['model_path']))
 
         #load optimzer
-        optimizer.load_state_dict(torch.load('./models/font_noStrokeStyle_Unet64_image64_2/optim_90.pt'))
+        optimizer.load_state_dict(torch.load(config['optim_path']))
         for state in optimizer.state.values():
             for k, v in state.items():
                 if isinstance(v, torch.Tensor):
@@ -127,7 +130,7 @@ if __name__ == '__main__':
         # Set model
         model = UNet().to(device)
         # Set optimizer
-        optimizer = optim.AdamW(model.parameters(), lr=lr)
+        optimizer = optim.AdamW(model.parameters(), lr=config['lr'])
 
     wandb.watch(model)
 
@@ -135,27 +138,28 @@ if __name__ == '__main__':
     loss_func = nn.MSELoss()
 
     #Set diffusion
-    diffusion = Diffusion(first_beta=1e-4,
-                          end_beta=0.02,
-                          noise_step=noise_step,
+    diffusion = Diffusion(first_beta=config['first_beta'],
+                          end_beta=config['end_beta'],
+                          noise_step=config['noise_step'],
                           beta_schedule_type='linear',
-                          img_size=input_size,
+                          img_size=config['input_size'],
                           device=device)
 
 
-    charAttar = CharAttar(num_classes=num_classes,device=device,style_path=style_path)
-    for epoch_id in range(n_epochs):
-        if resume_train:
-            epoch_id += start_epoch
+    charAttar = CharAttar(num_classes=config['num_classes'],device=device,style_path=style_path)
+    
+    for epoch_id in range(config['n_epochs']):
+        if config['resume_train']:
+            epoch_id += config['start_epoch']
 
-        print(f"Epoch {epoch_id}/{n_epochs} Train..")
+        print(f"Epoch {epoch_id}/{config['n_epochs']} Train..")
 
         train_pbar = tqdm(train_dataloader,desc=f"trian_{epoch_id}")
         tic = time()
         for i, (train_images, train_contents_ch, train_filename) in enumerate(train_pbar):
             train_images = train_images.to(device)
 
-            train_charAttr_list = charAttar.make_charAttr(train_images, train_contents_ch, mode=mode).to(device)
+            train_charAttr_list = charAttar.make_charAttr(train_images, train_contents_ch, mode=config['mode']).to(device)
 
             t = diffusion.sample_t(train_images.shape[0]).to(device)
             x_t, noise = diffusion.noise_images(train_images, t)
@@ -171,10 +175,9 @@ if __name__ == '__main__':
         train_pbar.set_postfix(MSE=loss.item())
         
         if epoch_id % 10 == 0 :
-            sampled_images = diffusion.portion_sampling(model, sampling_chars,charAttar=charAttar,sample_img=sample_img)
+            sampled_images = diffusion.portion_sampling(model, config['sampling_chars'],charAttar=charAttar,sample_img=sample_img,batch_size=config['batch_size'])
             model.train()
-            # sample_all_x = (sample_all_x.clamp(-1, 1) + 1) / 2
-            # sample_all_x = (sample_all_x * 255).type(torch.uint8)
+            
             # plot_images(sampled_images)
             # save_images(sample_all_x, os.path.join(result_image_path, f"{epoch_id}.jpg"))
             torch.save(model,os.path.join(result_model_path,f"model_{epoch_id}.pt"))
