31fe3dde4ba1049f1b1bc33e0a2c3768b7f99bed
o2rabbit <gih5444@naver.com>
2023-11-08 20:50:53 +0900

    Modify: enhance image processing

--- Backend/app/library/img_process.py
+++ Backend/app/library/img_process.py
@@ -1,57 +1,123 @@
 import cv2
-from PIL import Image, ImageDraw, ImageEnhance, ImageFont
+from PIL import Image, ImageFilter, ImageDraw, ImageEnhance, ImageFont
 import numpy as np
 import imutils
 
 
-class PreProcessing:
+class PreProcess:
 
     @staticmethod
-    def convert_PIL2CV(image: Image.Image) -> np.array:
+    def PIL2CV(image: Image.Image) -> np.array:
         """convert pillow image to opencv image, change color RGB -> BGR"""
         return cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
-    
+
     @staticmethod
-    def convert_CV2PIL(image: np.array) -> Image.Image:
+    def CV2PIL(image: np.array) -> Image.Image:
         """convert opencv image to pillow image, change color BGR -> RGB"""
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         return Image.fromarray(image)
 
     @staticmethod
-    def PIL_resize(image: Image.Image, size: tuple = (200, 200)) -> Image.Image:
-        return image.resize(size, Image.Resampling.LANCZOS)
+    def CV_resize(image: np.array, resize_width: int = 1200) -> np.array:
+        h, w, _ = image.shape
+        resize_ratio = resize_width / w # resize_width / original_width
+        resize_img = cv2.resize(image, dsize=(0,0), fx=resize_ratio, fy=resize_ratio, interpolation=cv2.INTER_LINEAR)
+        return resize_img
+    
+    @staticmethod
+    def PIL_resize(image: Image.Image, size: int = 200) -> Image.Image:
+        return image.resize((size, size), Image.Resampling.LANCZOS)
     
     @staticmethod
     def PIL_crop(image: Image.Image, crop_size: int = 10) -> Image.Image:
         h, w = image.size
         return image.crop((crop_size, crop_size, h-crop_size, w-crop_size))
-    
+
     @staticmethod
     def PIL_adjust_contrast(image: Image.Image, enhance_value: float = 1.) -> Image.Image:
         """
         Additional contrast adjustment for pillow image
         image (pillow image): input image
-        enhance_value : Determine contrast enhance level
+        enhance_value (int): Determine contrast enhance level
         """
         contrast_img = ImageEnhance.Contrast(image)
         return contrast_img.enhance(enhance_value)
+    
+    @staticmethod
+    def PIL_smoothing_edge(image: Image.Image, filter_size: int = 5) -> Image.Image:
+        """Smoothing alphabet letter edge"""
+        return image.filter(ImageFilter.ModeFilter(size=filter_size))
+    
+    @staticmethod
+    def detect_edge(image: np.array) -> np.array:
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        gray = cv2.GaussianBlur(gray, (11, 11), 0)
+        # Edge Detection.
+        canny = cv2.Canny(gray, 0, 200)
+        canny = cv2.dilate(canny, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5)))
+        return canny
+    
+    @staticmethod
+    def detect_contour(canny: np.array) -> list:
+        # Finding contours for the detected edges.
+        contours, hierarchy = cv2.findContours(canny, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+        contours = sorted(contours, key=cv2.contourArea, reverse=True)[:5] # Keeping only the largest detected contour.
+        return contours
 
     @staticmethod
+    def find_retangle_corners(contours: list) -> list:
+        """Find biggest retangle corners from contours"""
+        # Loop over the contours.
+        for c in contours:
+            epsilon = 0.02 * cv2.arcLength(c, True) # Approximate the contour.
+            corners = cv2.approxPolyDP(c, epsilon, True)
+            if len(corners) == 4: # If our approximated contour has four points
+                break
+
+        corners = sorted(np.concatenate(corners).tolist()) # Sorting the corners and converting them to desired shape.
+        return corners
+    
+    @staticmethod
+    def perspective_transform(image: np.array, corners: list, size: int = 400) -> np.array:
+        """perspective transform by corners"""
+        pts1 = np.float32(corners)
+        pts2 = np.float32([[0, 0], [0, size], [size, 0], [size, size]])
+
+        m = cv2.getPerspectiveTransform(pts1, pts2)
+        dst = cv2.warpPerspective(image, m, (size, size))
+        return dst
+    
+    @staticmethod
+    def morphology(image: np.array, kernel_shape: str = 'ellipse', kernel_size: tuple = (5, 5)):
+        assert kernel_shape.lower() == "ellipse" or "rect" or "cross"
+        if kernel_shape.lower() == "ellipse":
+            shape = cv2.MORPH_ELLIPSE
+        elif kernel_shape.lower() == "rect":
+            shape = cv2.MORPH_RECT
+        else:
+            shape = cv2.MORPH_CROSS
+
+        kernel = cv2.getStructuringElement(shape, kernel_size)
+        opening = cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel)
+        return opening
+    
+    @staticmethod
     def automatic_brightness_and_contrast(image: np.array,
-                                          alpha_mul: float = 1.,
-                                          beta_mul: float = 1.,
+                                          alpha_mul: int = 1.,
+                                          beta_mul: int = 1.,
                                           clip_hist_percent: int = 1
                                           ) -> np.array:
         """
-        Automatically adjust image brightness and contrast.
-        alpha_mul (float): alpha(brightness) multiply param to adjust background color white. higher value make image more bright. default=1.
-        beta_mul (float): beta(contrast) multiply param to adjust letter more vivid. higher value make image more contrast. defulat=1.
+        adjust image brightness and contrast automaticully.
+        image (numpy array): input image 
+        alpha_mul (int): alpha(brightness) multiply param to adjust background color white. default=1.5 (experiment value)
+        beta_mul (int): beta(contrast) multiply param to adjust letter more vivid. default=1.5 (experiment value)
         clip_hist_percent (int): cumulative distribution to determine where color frequency is less than threshold value. default=1(%)
         """
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         
         # Calculate grayscale histogram
-        hist = cv2.calcHist([gray],[0],None,[256],[0,256])
+        hist = cv2.calcHist([gray], [0], None, [256], [0,256])
         hist_size = len(hist)
         
         # Calculate cumulative distribution from the histogram
@@ -77,83 +143,49 @@ class PreProcessing:
         
         # Calculate alpha and beta values
         alpha = 255 / (maximum_gray - minimum_gray)
-        beta = (-minimum_gray * alpha) * beta_mul
-        alpha = alpha * alpha_mul
+        beta = -minimum_gray * alpha
 
-        auto_result = cv2.convertScaleAbs(image, alpha=alpha, beta=beta)
+        auto_result = cv2.convertScaleAbs(image, alpha=alpha*alpha_mul, beta=beta*beta_mul)
         return auto_result
 
-    @staticmethod
-    def perspective_transform(image: np.array, 
-                              return_img_size: int = 200, 
-                              inner_resize_width: int = 1200,
-                              ) -> np.array:
-        """
-        Detect Retangle and PerciveTransform Processing.
-        image (pillow Image): input image
-        return_img_size (int): return image size
-        inner_resize_width (int): inner resize value for detect retangle in image. default = 1200 (experiment value)
-        """
-        # assert type(image_array)
-        size = return_img_size
-        
-        h, w, _ = image.shape
-        resize_ratio = inner_resize_width / w
-        resize_img = cv2.resize(image, dsize=(0,0), fx=resize_ratio, fy=resize_ratio, interpolation=cv2.INTER_LINEAR)
-
-        # # convert the resize_img to grayscale, blur, and find edges
-        gray = cv2.cvtColor(resize_img, cv2.COLOR_BGR2GRAY)
-        gray = cv2.GaussianBlur(gray, (5, 5), 0)
-        edged = cv2.Canny(gray, 75, 200)
-
-        # find contours in the edge map, then initialize
-        # the contour that corresponds to the document
-        cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        cnts = imutils.grab_contours(cnts)
-        doc_cnt = None
-        # ensure that at least one contour was found
-        if len(cnts) > 0:
-            # sort the contours according to their size in
-            # descending order
-            cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
-            # loop over the sorted contours
-            for c in cnts:
-                # approximate the contour
-                peri = cv2.arcLength(c, True)
-                approx = cv2.approxPolyDP(c, 0.02 * peri, True)
-                # if our approximated contour has four points,
-                # then we can assume we have found the paper
-                if len(approx) == 4:
-                    doc_cnt = approx
-                    break
-        
-        # perspective transform
-        pts1 = np.float32([doc_cnt[0][0], doc_cnt[1][0], doc_cnt[3][0], doc_cnt[2][0]])
-        pts2 = np.float32([[0, 0], [0, size], [size, 0], [size, size]])
-
-        m = cv2.getPerspectiveTransform(pts1, pts2)
-        dst = cv2.warpPerspective(resize_img, m, (size, size))
-        return dst
     
 
-def pre_processing(image: Image.Image,
-                   size: int = (200, 200),
-                   edge_crop_size: int = 10,
-                   brightness_adj_val: float = 1.,
-                   contrast_adj_val: float = 1.,
-                   contrast_enhance_val: float = 1.,
-                   ) -> Image.Image:
+def image_processing(image: Image.Image,
+                     size: int = 200,
+                     edge_crop_size: int = 10,
+                     brightness_adj: float = 1.5,
+                     contrast_enhance: float = 2,
+                     inner_resize_w: int = 1200
+                     ) -> Image.Image:
     """
     Image Pre-Processing Function.
     """
-    image = PreProcessing.convert_PIL2CV(image)
-    image = PreProcessing.perspective_transform(image, return_img_size=size[0]*2, inner_resize_width=1200)
-    image = PreProcessing.automatic_brightness_and_contrast(image, alpha_mul=brightness_adj_val, beta_mul=contrast_adj_val)
-    image = PreProcessing.convert_CV2PIL(image)
-    image = PreProcessing.PIL_adjust_contrast(image, enhance_value=contrast_enhance_val)
-    image = PreProcessing.PIL_crop(image, crop_size=edge_crop_size)
-    image = PreProcessing.PIL_resize(image, size=size)
+    image = image.convert("L")
+    image = PreProcess.PIL2CV(image)
+    image = PreProcess.CV_resize(image, resize_width=inner_resize_w)
+
+    # detect retangle and find retangle corner
+    canny = PreProcess.detect_edge(image)
+    contour = PreProcess.detect_contour(canny)
+    corner = PreProcess.find_retangle_corners(contour)
+
+    # perspective_transform
+    image = PreProcess.perspective_transform(image, corner, size*2)
+
+    #adjust image morp and color
+    image = PreProcess.automatic_brightness_and_contrast(image, alpha_mul=brightness_adj)
+    image = PreProcess.morphology(image, kernel_shape='ellipse', kernel_size=(5, 5))
+
+    # convert cv image to pillow and adjust image color 
+    image = PreProcess.CV2PIL(image)
+    image = PreProcess.PIL_adjust_contrast(image, enhance_value=contrast_enhance)
+    image = PreProcess.PIL_smoothing_edge(image, filter_size=5)
+
+    # crop edge and resize
+    image = PreProcess.PIL_crop(image, crop_size=edge_crop_size)
+    image = PreProcess.PIL_resize(image, size=size)
     return image
+    
 
 
 def make_example_from_ttf(text: str,
