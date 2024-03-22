5da81df2ed79cd5a9378da095fb514cf6b81b35a
o2rabbit <gih5444@naver.com>
2024-03-22 15:05:59 +0900

    Modify: not use subset

--- Ko_diffusion/train.py
+++ Ko_diffusion/train.py
@@ -46,12 +46,14 @@ test_name = "font_dataset_test"
 train_dirs = "H:/data/Hangul_Chars_64_420_Gray_font"
 # sample_img_path_1 = f'{train_dirs}/갊/62570_갊.png'
 # sample_img_path_2 = f'{train_dirs}/갊/나눔손글씨김유이체_갊.png'
+sample_img_len = 8
 sample_img_path_1 = f"{train_dirs}/NanumGothicBold/NanumGothicBold_갊.png"
 sample_img_path_2 = f"{train_dirs}/나눔손글씨김유이체/나눔손글씨김유이체_갊.png"
 stroke_text_path = "./text_weight/storke_txt.txt"
 style_enc_path = "./text_weight/korean_styenc.ckpt"
 
 start_epoch = 0
+change_cond_mode = n_epochs // 2
 
 # os
 os.environ['CUDA_LAUNCH_BLOCKING'] = "1"
@@ -105,9 +107,6 @@ if __name__ == '__main__':
     os.environ['CUDA_VISIBLE_DEVICES'] = str(gpu_num)
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-
-    # train_dirs = "H:/data/hangle_image64_gray_small"
-
     # Set transform
     img_transforms = torchvision.transforms.Compose([
         torchvision.transforms.Grayscale(num_output_channels=1),
@@ -138,8 +137,8 @@ if __name__ == '__main__':
     #sample_img
     sample_img_1, sample_img_2 = Image.open(sample_img_path_1).convert("RGB"), Image.open(sample_img_path_2).convert("RGB")
     sample_img_1, sample_img_2 = sample_transforms(sample_img_1).to(device), sample_transforms(sample_img_2).to(device)
-    sample_img_1, sample_img_2 = sample_img_1.unsqueeze(dim=0), sample_img_1.unsqueeze(dim=0)
-    sample_img_1, sample_img_2 = sample_img_1.repeat(8, 1, 1, 1), sample_img_2.repeat(8, 1, 1, 1)
+    sample_img_1, sample_img_2 = sample_img_1.unsqueeze(dim=0), sample_img_2.unsqueeze(dim=0)
+    sample_img_1, sample_img_2 = sample_img_1.repeat(sample_img_len, 1, 1, 1), sample_img_2.repeat(sample_img_len, 1, 1, 1)
 
 
     #Set model
@@ -198,13 +197,13 @@ if __name__ == '__main__':
     for epoch_id in range(start_epoch,n_epochs):
         print(f"Epoch {epoch_id}/{n_epochs} Train..")
 
-        if cond_mode == 3 and epoch_id > 400:
+        if cond_mode == 3 and epoch_id > change_cond_mode:
             cond_mode = 1
         
         pbar = tqdm(dataloader, desc=f"trian_{epoch_id}", ncols=120)
         tic = time()
         # for i, (image, content) in enumerate(pbar):
-        for content, image, sty_image in pbar:
+        for i, (content, image, sty_image) in enumerate(pbar):
             # print('x1 : ', x.shape)
             image, sty_image = image.to(device), sty_image.to(device)
             # condition = make_condition.make_condition(images = image,indexs = content,mode=1).to(device)
@@ -222,22 +221,26 @@ if __name__ == '__main__':
             ema_model.update_parameters(model)
             scheduler.step()
 
+            if i % (len(dataloader) // 10) == 0:
+                wandb.log({"train_mse_loss": loss}, commit=True)
+
         toc = time()
-        wandb.log({"train_mse_loss": loss,'train_time':toc-tic}, commit=True)
+        # wandb.log({"train_mse_loss": loss,'train_time':toc-tic}, commit=True)
+        wandb.log({'train_time':(toc-tic)//60, "custom_epoch": epoch_id}, commit=True)
         pbar.set_postfix(MSE=loss.item())
 
-        if epoch_id % 10 == 0 :
+        # if epoch_id % 10 == 0 :
         # Save
-            labels = torch.arange(num_classes).long().to(device)
-            sampled_images1 = diffusion.portion_sampling(ema_model, n=len(dataset.classes), sampleImage_len=8, sty_img_1=sample_img_1, sty_img_2=sample_img_2, make_condition=make_condition, log_name="EMA")
-            sampled_images2 = diffusion.portion_sampling(model, n=len(dataset.classes), sampleImage_len=8, sty_img_1=sample_img_1, sty_img_2=sample_img_2, make_condition=make_condition, log_name="None-EMA")
-            # plot_images(sampled_images)
-            save_images(sampled_images1, os.path.join(result_image_path, f"{epoch_id}.jpg"))
-            save_images(sampled_images2, os.path.join(result_image_path, f"{epoch_id}.jpg"))
-            torch.save(model,os.path.join(result_model_path,f"model_2_{epoch_id}.pt"))
-            torch.save(model.state_dict(), os.path.join(result_model_path, f"ckpt_2_{epoch_id}.pt"))
-            torch.save(ema_model.state_dict(), os.path.join(result_model_path, f"ema_ckpt_2_{epoch_id}.pt"))
-            torch.save(optimizer.state_dict(), os.path.join(result_model_path, f"optim_2_{epoch_id}.pt"))
+        labels = torch.arange(num_classes).long().to(device)
+        sampled_images1 = diffusion.portion_sampling(ema_model, n=len(dataset.classes), sampleImage_len=sample_img_len, sty_img_1=sample_img_1, sty_img_2=sample_img_2, make_condition=make_condition, log_name="EMA")
+        sampled_images2 = diffusion.portion_sampling(model, n=len(dataset.classes), sampleImage_len=sample_img_len, sty_img_1=sample_img_1, sty_img_2=sample_img_2, make_condition=make_condition, log_name="None-EMA")
+        # plot_images(sampled_images)
+        save_images(sampled_images1, os.path.join(result_image_path, f"{epoch_id}.jpg"))
+        save_images(sampled_images2, os.path.join(result_image_path, f"{epoch_id}.jpg"))
+        torch.save(model,os.path.join(result_model_path,f"model_2_{epoch_id}.pt"))
+        torch.save(model.state_dict(), os.path.join(result_model_path, f"ckpt_2_{epoch_id}.pt"))
+        torch.save(ema_model.state_dict(), os.path.join(result_model_path, f"ema_ckpt_2_{epoch_id}.pt"))
+        torch.save(optimizer.state_dict(), os.path.join(result_model_path, f"optim_2_{epoch_id}.pt"))
 
     # Update bn statistics for the ema_model at the end ???
     torch.optim.swa_utils.update_bn(dataloader, ema_model)
